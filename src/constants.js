b15138d4d38cec7d654c66dbc60ffa5a943895ab
Eric Cheng <eric.bc8@gmail.com>
2025-09-28 02:59:58 -0400

    pin to latest

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@
 // Nonthinking model, sometimes doesn't explain well
 // export const GEMINI_MODEL = 'gemini-2.5-flash-lite';
 // Thinking and fast, good middle ground
-export const GEMINI_MODEL = 'gemini-2.5-flash-preview-09-2025';
+export const GEMINI_MODEL = 'gemini-flash-latest';
 // Kind of slow and lower limits, but great response
 // export const GEMINI_MODEL = 'gemini-2.5-pro';
 
